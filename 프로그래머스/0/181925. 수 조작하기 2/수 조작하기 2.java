365b417b3d76ea0a9a3a98ada1abae9f643faf28
yongjae730 <dldydwo19l23@gmail.com>
2025-06-18 16:27:26 +0900

    [level 0] Title: 수 조작하기 2, Time: 1126.10 ms, Memory: 380 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        
+        for (int i = 1; i < numLog.length; i++) {
+            
+            int diff = numLog[i]-numLog[i-1] ;    
+            switch (diff) {
+                case 1 :
+                    answer += "w";
+                    break;
+                case -1 :
+                    answer += "s";
+                    break;
+                case 10 :
+                    answer += "d";
+                    break;
+                case -10 :
+                    answer += "a";
+                    break;
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
