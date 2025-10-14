ea1639da540cf5ca4a368951a263cebb9971f034
yongjae730 <dldydwo19l23@gmail.com>
2025-10-14 21:09:47 +0900

    [Bronze V] Title: 직사각형, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+
+public class Main { // 백준 27323번 문제, 직사각형 (기하 1: 직사각형과 삼각형)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        System.out.println(a * b);
+
+    }
+}
